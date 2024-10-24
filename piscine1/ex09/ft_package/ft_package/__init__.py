ad60ed1b10144137745534975d34948e436c705a
MrMobbi <julliatmathias06@gmail.com>
2024-10-24 10:37:06 +0200

    ex09 added

---
+++ piscine1/ex09/ft_package/ft_package/__init__.py
@@ -0,0 +1,7 @@
+
+
+from .building import display_characters
+from .find_ft_type import all_thing_is_obj
+from .ft_filter import ft_filter
+from .Loading import ft_tqdm
+from .NULL_not_found import NULL_not_found
---
+++ piscine1/ex09/tester.py
@@ -0,0 +1,8 @@
+
+from time import sleep
+from ft_package import ft_tqdm
+
+
+for elem in ft_tqdm(range(10)):
+    sleep(0.1)
+print()
