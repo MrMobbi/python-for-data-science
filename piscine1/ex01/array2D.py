ad8b0af3d1a5ba1d5e652e39fd0a3c33ad8d8964
MrMobbi <julliatmathias06@gmail.com>
2024-10-31 21:06:52 +0100

    docstring added

--- piscine1/ex01/array2D.py
+++ piscine1/ex01/array2D.py
@@ -3,7 +3,11 @@ import numpy as np
 
 
 def slice_me(familly: list, start: int, end: int) -> list:
-    """ """
+    """def slice_me(familly: list, start: int, end: int) -> list:
+
+    Prints the shape of the 2D array and returns a truncated version of the array
+    based on the specified row indices. The slicing method is used to obtain the
+    sub-array from start_row to end_row."""
 
     if not isinstance(familly, list):
         raise ValueError("Familly must be a list")
---
+++ piscine1/ex01/tester.py
@@ -0,0 +1,10 @@
+
+from array2D import slice_me
+
+
+def main():
+    print(slice_me.__doc__)
+
+
+if __name__ == "__main__":
+    main()
