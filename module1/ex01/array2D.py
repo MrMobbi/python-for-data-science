148f6b2b2f600eb4c9f6030a9affefbeb2531f91
MrMobbi <julliatmathias06@gmail.com>
2024-11-05 13:19:14 +0100

    ex01 done

--- module1/ex01/array2D.py
+++ module1/ex01/array2D.py
@@ -9,10 +9,43 @@ def slice_me(familly: list, start: int, end: int) -> list:
     based on the specified row indices. The slicing method is used to obtain the
     sub-array from start_row to end_row."""
 
+    # --- Test for the list ---
+    if not familly:
+        raise ValueError("List cannot be empty")
+
     if not isinstance(familly, list):
         raise ValueError("Familly must be a list")
 
-    if start >= len(familly) or end >= len(familly) or len(familly) + end < 0:
+    first_size = len(familly[0])
+    for item in familly:
+        if not isinstance(item, list):
+            raise ValueError("Item in the list is not a list")
+        if first_size != len(item):
+            raise ValueError("List are not the same size")
+
+    # --- Test for the end and start ---
+    if start < 0:
+        start = len(familly) + start
+    if end < 0:
+        end = len(familly) + end
+
+#   print(f"start {start} |  end {end}")
+
+    if start >= len(familly) or end >= len(familly):
         raise ValueError("Start and End must be in the range of familly")
 
-    print(f"")
+    if start > end:
+        raise ValueError("Start cannot be greater than end")
+
+    if start < 0 or end < 0:
+        raise ValueError("Start or end cannot be bellow 0")
+
+    if start == end:
+        raise ValueError("Start and end cannot be the same")
+
+    print(f"My shape is : ({len(familly)}, {len(familly[0])})")
+    inital_array = np.array(familly)
+    new_array = inital_array[start:end]
+    new_list = new_array.tolist()
+    print(f"My new shape is : ({len(new_list)}, {len(new_list[0])})")
+    return new_list
--- module1/ex01/tester.py
+++ module1/ex01/tester.py
@@ -3,7 +3,14 @@ from array2D import slice_me
 
 
 def main():
-    print(slice_me.__doc__)
+    test = [
+        [1.80, 'coucou'],
+        [2.15, 102.7],
+        [2.10, 98.5],
+        [1.88, 75.2],
+    ]
+    print(slice_me(test, 0, 2))
+    print(slice_me(test, 1, -2))
 
 
 if __name__ == "__main__":
