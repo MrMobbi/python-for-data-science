718ce95be0ca9f6344395cca8ce01dd881289843
MrMobbi <julliatmathias06@gmail.com>
2024-11-07 18:53:19 +0100

    ex04 added

--- module1/ex04/rotate.py
+++ module1/ex04/rotate.py
@@ -20,7 +20,31 @@ def main():
     right = ((original_width + crop_width) // 2) + 130
 
     cropped_img = img.crop((left, top, right, bottom))
-    image_to_zoom = np.array(cropped_img)
+    array_cropped = np.array(cropped_img)
+
+    height, width = cropped_img.size
+    angle = 90
+    pi = 3.141592653589793
+    angle_rad = angle * pi / 180.0
+    center_x, center_y = width / 2, height / 2
+
+    cos = np.cos(angle_rad)
+    sin = np.sin(angle_rad)
+
+    rotated_image = np.zeros_like(array_cropped)
+
+    for y in range(height):
+        for x in range(width):
+            translated_x = x - center_x
+            translated_y = y - center_y
+
+            rotated_x = int(translated_x * cos - translated_y * sin + center_x)
+            rotated_y = int(translated_x * sin + translated_y * cos + center_y)
+
+            if 0 <= rotated_x < width and 0 <= rotated_y < height:
+                rotated_image[rotated_y, rotated_x] = array_cropped[y, x]
+
+    image_to_zoom = rotated_image
     print(f"New shape after slicing : {image_to_zoom.shape}")
     print(image_to_zoom[3])
     plt.imshow(image_to_zoom)
